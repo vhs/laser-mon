37ff7ea16436b9308f757895ece8a5ab47ca3c2f
Logan Buchy <logan.buchy@gmail.com>
2015-12-08 00:49:44 -0800

    Add N-buffering scheme to DataSampler

--- include/DataSamples.hpp
+++ include/DataSamples.hpp
@@ -2,6 +2,7 @@
 #define DATA_SAMPLES_H
 
 #include <stdint.h>
+#include <atomic>
 
 /**
  * Holds the data during a sampling period
@@ -24,6 +25,41 @@ struct SampleData
   uint32_t numTtlPinHigh;
 };
 
+template <class T, uint32_t NumBuf = 3>
+class RwBuffer
+{
+public:
+	// This class is neither thread-safe nor necessarily correct (due to potentially incorrect use of atomics)
+	// However it is used in interrupt code so mutexes are not really an option
+	RwBuffer()
+		: m_readIdx(0)
+	{}
+
+	T const& GetReadBuf() const
+	{
+		return m_buffer[m_readIdx];
+	}
+
+	T& GetWriteBuf()
+	{
+		unsigned int const writeIdx = (m_readIdx + 1) % NumBuf;
+		return m_buffer[writeIdx]; // TODO verify if this actually works
+	}
+
+	void Flip()
+	{
+		uint32_t readIdx = m_readIdx;
+		readIdx = (readIdx + 1) % NumBuf;
+		// Atomically flip the readIdx
+		m_readIdx.store(readIdx);
+	}
+
+private:
+	std::atomic<unsigned int> m_readIdx;
+	T m_buffer[NumBuf];
+
+};
+
 /**
  * Handles the state of SampleData
  * This will sample the PWM and TTL pins and accumulate their results in SampleData
@@ -34,38 +70,47 @@ class DataSampler
 {
 public:
   DataSampler()
+	: m_bHasBeenReset(false)
   {}
 
   void Reset()
   {
+	m_sampleDataBuf.Flip();
+	SampleData& sampleData = m_sampleDataBuf.GetWriteBuf();
     // we don't care about startMillis - we will just reset it when adding
     // the first sample
-    m_sampleData.numMeasurements = 0;
-    m_sampleData.numPwmPinHigh = 0;
-    m_sampleData.numTtlPinHigh = 0;
+	sampleData.numMeasurements = 0;
+	sampleData.numPwmPinHigh = 0;
+	sampleData.numTtlPinHigh = 0;
+
+	m_bHasBeenReset = true;
   }
 
   void DoSample(uint8_t pwmPin, uint8_t ttlPin)
   {
-    if (m_sampleData.numMeasurements == 0)
+	SampleData& sampleData = m_sampleDataBuf.GetWriteBuf();
+    if (sampleData.numMeasurements == 0)
     {
       // This is our first measurement
-      m_sampleData.startMillis = millis();
+    	sampleData.startMillis = millis();
     }
     int pwmVal = digitalRead(pwmPin);
     int ttlVal = digitalRead(ttlPin);
-    m_sampleData.numPwmPinHigh += (pwmVal) ? 1 : 0;
-    m_sampleData.numTtlPinHigh += (ttlVal) ? 1 : 0;
-    m_sampleData.numMeasurements++;
+    sampleData.numPwmPinHigh += (pwmVal) ? 1 : 0;
+    sampleData.numTtlPinHigh += (ttlVal) ? 1 : 0;
+    sampleData.numMeasurements++;
   }
 
   SampleData GetData() const
   {
-    return m_sampleData;
+	if (!m_bHasBeenReset)
+		return SampleData();
+    return m_sampleDataBuf.GetReadBuf();
   }
 
 private:
-  SampleData m_sampleData;
+  RwBuffer<SampleData> m_sampleDataBuf;
+  bool m_bHasBeenReset;
 };
 
 #endif
